62cb5fc2e7fdb2244185ee2fe77a18bf9abdae40
LakeCombs <adesolaolamilekan2@gmail.com>
2022-12-19 23:57:52 +0100

    added the progress bar

--- components/transactionComponent/progressBar.js
+++ components/transactionComponent/progressBar.js
@@ -1,36 +1,24 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const ProgressBar = () => {
+	const { page } = useSelector((state) => state.checkoutScreen);
 	return (
-		<div className="container">
-			<div className="wrapper">
-				<div className="arrow-steps clearfix">
-					<div className="step current">
-						{" "}
-						<span> Step 1</span>{" "}
-					</div>
-					<div className="step">
-						{" "}
-						<span>Step 2 some words</span>{" "}
-					</div>
-					<div className="step">
-						{" "}
-						<span> Step 3</span>{" "}
-					</div>
-					<div className="step">
-						{" "}
-						<span>Step 4</span>{" "}
-					</div>
+		<div className="mb-5 flex flex-wrap w-full  text-white">
+			{[
+				"1. Order Water-Meter",
+				"2. Review Order",
+				"3. Payment Information",
+				"4. Complete Order",
+			].map((step, index) => (
+				<div
+					key={step}
+					className={`flex-1 text-center justify-center p items-center flex ${
+						index === page ? "primaryBlueBG  py-2" : "bg-[#7E7E81]  py-3"
+					}`}>
+					{step}
 				</div>
-				<div className="nav clearfix">
-					<a href="#" className="prev">
-						Previous
-					</a>
-					<a href="#" className="next pull-right">
-						Next
-					</a>
-				</div>
-			</div>
+			))}
 		</div>
 	);
 };
